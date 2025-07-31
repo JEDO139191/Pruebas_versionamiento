d68d357d95baf552af3ebc34c0e66b04e4d52f32
JEDO139191 <jedo139191@gmail.com>
2025-07-31 13:28:55 -0600

    Versión 4: Función despedir

--- Main.kt
+++ Main.kt
@@ -2,8 +2,13 @@ fun saludar(nombre: String) {
     println("Hola, $nombre")
 }
 
+fun despedir(nombre: String) {
+    println("Adiós, $nombre")
+}
+
 fun main() {
     print("¿Cuál es tu nombre? ")
     val nombre = readLine() ?: ""
     saludar(nombre)
+    despedir(nombre)
 }
