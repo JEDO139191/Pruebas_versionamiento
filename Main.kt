e97fb0629a214115544dbb5adb4510208c83721b
JEDO139191 <jedo139191@gmail.com>
2025-07-31 13:29:31 -0600

    Versión 5: Maneja nombre vacío

--- Main.kt
+++ Main.kt
@@ -1,14 +1,22 @@
 fun saludar(nombre: String) {
-    println("Hola, $nombre")
+    if (nombre.isNotBlank()) {
+        println("Hola, $nombre")
+    } else {
+        println("Hola, desconocido")
+    }
 }
 
 fun despedir(nombre: String) {
-    println("Adiós, $nombre")
+    if (nombre.isNotBlank()) {
+        println("Adiós, $nombre")
+    } else {
+        println("Adiós, desconocido")
+    }
 }
 
 fun main() {
     print("¿Cuál es tu nombre? ")
-    val nombre = readLine() ?: ""
+    val nombre = readLine()?.trim() ?: ""
     saludar(nombre)
     despedir(nombre)
 }
